3a59b686aaa53e71997e2e35ed561ec453b2a21e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-20 15:06:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,7 @@ def turn_count
 
 def won?
   WIN_COMBINATIONS.detect do |win_combo|
-    
+
     if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
       return win_combo
     elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
@@ -92,7 +92,7 @@ def play
   elsif draw?
     puts "Cats Game!"
   end 
-    
+
 end
 
 end
